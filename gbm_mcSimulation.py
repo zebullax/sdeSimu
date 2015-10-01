d667c2005b37b7d9f50afe48d514d4782f1dd31f
zebullax <zebullax@gmail.com>
2015-10-01 15:32:00 +0900

    poisson process

--- gbm_mcSimulation.py
+++ gbm_mcSimulation.py
@@ -21,12 +21,28 @@ def simuGBM(simuParams) :
         simuVal.append(lastVal * math.exp(mu*dt + sigma*math.sqrt(dt)*random.normalvariate(0,sigma)))
     return simuVal
 
+# Simulate jump times for poisson process
+# with intensity l
+def simuPoissonProcess(simuParams) : 
+    t       = 0
+    dt      = simuParams["dt"]
+    l       = simuParams["l"]
+    T       = simuParams["T"]
+    jumps   = []
+    F       = 1 - math.exp(-l*dt)
+        
+    while t <= T :         
+        if (random.uniform(0,1) <= F) :
+            jumps.append(t)
+        t = t + dt
+    return jumps
+
 def outputSimuToFile(filename, simuResult) : 
     outFile = open(filename, mode="w")    
     for val in simuResult : 
         print(val, sep=",", file=outFile)
 
-def outputSimuToPlot(simuParams, simuResult) : 
+def outputSimuGbmToPlot(simuParams, simuResult) : 
     T       = simuParams["T"]
     dt      = simuParams["dt"]    
     nbSteps = int(T/dt + 1) # + 1 for x0
@@ -38,8 +54,22 @@ def outputSimuToPlot(simuParams, simuResult) :
     plt.ylabel("S(t)")
     plt.show()
      
+def outputSimuPoissonToPlot(simuParams, simuResult) : 
+    T       = simuParams["T"]
+    dt      = simuParams["dt"]         
+    jumpT   = numpy.asarray(simuResult)
+    
+    for time in jumpT : 
+        plt.plot(time,1,"o")
+    plt.xlabel("time(y)")
+    plt.ylabel("N(t)")
+    plt.show()
+
 def main() :
     argparser = argparse.ArgumentParser(prog = "Monte Carlo simulation of GBM", add_help = True)
+    #GBM Params
+    argparser.add_argument("--gbm", type=bool, default=False,\
+        help="Simulate geometric Brownian motion")    
     argparser.add_argument("--s", "--std", type=float, default=0.2,\
         help="Annual standard deviation (coefficient for the dWt term)")
     argparser.add_argument("--m", "--mean", type=float, default=0.0,\
@@ -47,21 +77,34 @@ def main() :
     argparser.add_argument("--i", "--init", type=float, default=100.0,\
         help="Initial value for the process")
     argparser.add_argument("--T", "--maturity", type=float, default=1.0,\
-        help="Upper time value (unit year) for the simulation")
+        help="End time (unit year) for the simulation")
     argparser.add_argument("--dt", "--timestep", type=float, default=0.01,\
-        help="Timestep value used for time discretization")
+        help="Timestep used for time discretization")
+    #Poisson Params
+    argparser.add_argument("--poisson", type=bool, default=True,\
+        help="Simulate Poisson process")
+    argparser.add_argument("--l", "--lambda", type=float, default=10.0,\
+        help="Jump intensity for Poisson process")
+    #Output params
     argparser.add_argument("--o", "--outFile", type=str, \
-        help="Comma delimited file for simulation output results")
+        help="CSV filename for simulation results")
     argparser.add_argument("--g", "--outGraph", type=bool, default=True,\
-        help="Comma delimited file for simulation output results")
+        help="Plot simulation results")
    
-    args        = vars(argparser.parse_args())    
-    simuResult  = simuGBM(args)
+    args = vars(argparser.parse_args())
+    if args["poisson"] : 
+        simuResult  = simuPoissonProcess(args)
+    elif args["gbm"] :
+        simuResult  = simuGBM(args)
+
 
     if args["o"] :
         outputSimuToFile(args["o"],simuResult)
     if args["g"] :
-        outputSimuToPlot(args, simuResult)
+        if args["gbm"] : 
+            outputSimuGbmToPlot(args, simuResult)
+        elif args["poisson"] : 
+            outputSimuPoissonToPlot(args, simuResult)
             
 if __name__ == "__main__" :
         main()
